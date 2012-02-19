6a764d095215c9047c08d670dd8be0b2a79cb285
Ustimenko.Alexander <Ustimenko.Alexander@gmail.com>
2012-02-19 23:35:54 +0700

    Initial commit

---
+++ 00-load-data-mysql.sql
@@ -0,0 +1,27 @@
+drop database if exists geoip_flat_range;
+create database geoip_flat_range;
+use geoip_flat_range;
+
+-- ip_start, ip_end, range_start, range_end, country_code
+
+create table countries_ips (
+	ip_start char(15) not null default '0.0.0.0',
+	ip_end char(15) not null default '0.0.0.0',
+	range_start int unsigned not null default 0,
+	range_end int unsigned not null default 0,
+	country_code char(2) not null default '',
+	primary key (range_start, range_end, country_code)
+) engine=MyISAM;
+
+/**
+ * @see http://www.maxmind.com/app/geoip_country
+ * Change to your local path
+ */
+load data infile 'D:\\Docs\\Private\\Publication\\GeoIP\\geoip-flat-range\\GeoIPCountryWhois.csv'
+into table countries_ips columns terminated by ',' enclosed by '"';
+
+alter table countries_ips
+	drop column ip_start,
+	drop column ip_end;
+
+select * from countries_ips limit 10;
